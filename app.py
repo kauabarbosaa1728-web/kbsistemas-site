387befa3b1bad3a250be4202ad908abe518a3d7f
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-07-27 17:46:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,41 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 # Página inicial
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
-# Sobre
+
+# Sobre a KBSistemas
 @app.route("/sobre")
 def sobre():
     return render_template("sobre.html")
 
-# Serviços
+
+# Serviços (sistemas, sites, aplicativos)
 @app.route("/servicos")
 def servicos():
     return render_template("servicos.html")
 
-# Planos
+
+# Planos e soluções
 @app.route("/planos")
 def planos():
     return render_template("planos.html")
+
 
 # Contato
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+
+# Orçamento
+@app.route("/orcamento")
+def orcamento():
+    return render_template("orcamento.html")
 
 
 if __name__ == "__main__":
